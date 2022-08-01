fa56d6f7e0895208299b8807c81d4cff31aca24f
djam388 <djam388@hotmail.com>
2022-08-01 16:48:38 +0500

    Attempt #1

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,32 @@
 package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
-    public void isLeapYear(int year) {
+    public static void main(String[] args) {
+        isLeapYear(1922);
+    }
+    public static void isLeapYear(int year) {
+        switch (year % 400) {
+            case 0:
+                System.out.println("leap");
+                break;
+            default:
+                switch ((year % 100)) {
+                    case 0:
+                        System.out.println("not leap");
+                        break;
+                    default:
+                        switch (year % 4) {
+                            case 0:
+                                System.out.println("leap");
+                                break;
+                            default:
+                                System.out.println("not leap");
+                                break;
+                        }
+                        break;
+                }
+                break;
+        }
+
     }
 }
